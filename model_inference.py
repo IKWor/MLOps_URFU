d36bbf3acc702786853470aef5889118c3808fa1
IKWor <149700414+IKWor@users.noreply.github.com>
2024-04-02 11:30:20 +0300

    Create model_inference.py

---
+++ model_inference.py
@@ -0,0 +1,10 @@
+import joblib
+
+# Загрузка обученной модели
+model = joblib.load('trained_model.pkl')
+
+# Пример использования модели для предсказания на новых данных
+new_data = pd.DataFrame([[1, 2, 3]], columns=['feature1', 'feature2', 'feature3'])
+prediction = model.predict(new_data)
+
+print(f'Prediction for new data: {prediction}')
