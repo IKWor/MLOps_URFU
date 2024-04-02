ddf20f6792e3a9fc8f59c3e7bdf36fd7e338f893
IKWor <149700414+IKWor@users.noreply.github.com>
2024-04-02 11:29:52 +0300

    Create model_evaluation.py

---
+++ model_evaluation.py
@@ -0,0 +1,16 @@
+from sklearn.metrics import mean_squared_error
+import joblib
+import pandas as pd
+
+# Загрузка обученной модели
+model = joblib.load('trained_model.pkl')
+
+# Загрузка тестовых данных
+test_data = pd.read_csv('test/test_data.csv')
+
+# Предсказание с помощью модели
+predictions = model.predict(test_data)
+
+# Оценка модели с использованием среднеквадратичной ошибки
+mse = mean_squared_error(true_values, predictions)
+print(f'Mean Squared Error: {mse}')
