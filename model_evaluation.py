f5fc7c34f4711201742da997b458bfe6502a3e13
IKWor <149700414+IKWor@users.noreply.github.com>
2024-04-02 11:52:31 +0300

    Update model_evaluation.py

--- model_evaluation.py
+++ model_evaluation.py
@@ -11,6 +11,11 @@ test_data = pd.read_csv('test/test_data.csv')
 # Предсказание с помощью модели
 predictions = model.predict(test_data)
 
+true_values = [1, 2, 3, 4, 5]
+predictions = [1.2, 2.3, 3.5, 4.1, 5.2]
+
+mse = mean_squared_error(true_values, predictions)
+
 # Оценка модели с использованием среднеквадратичной ошибки
 mse = mean_squared_error(true_values, predictions)
 print(f'Mean Squared Error: {mse}')
